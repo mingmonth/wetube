755da941f7e8ba5b0aa5d98220fee8d9ea60ebc4
mingmonth <ys9922@naver.com>
2019-02-26 00:16:06 +0900

    #2.10 MVC Pattern part Two

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter";
-import videoRouter from "./routers/videoRouter"
-import globalRouter from "./routers/videoRouter"
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 const app = express();
 
 app.use(cookieParser());
@@ -14,8 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(morgan("tiny"));
 app.use(helmet());
 
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
