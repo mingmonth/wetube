51e06871b1abca561656474a2bff3dd50ffb9aa5
yoonsoo <yoonsoo@hknetworks.kr>
2019-02-26 21:06:04 +0900

    #2.13 Installing Pug

--- app.js
+++ app.js
@@ -9,9 +9,10 @@ import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 const app = express();
 
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("tiny"));
 app.use(helmet());
 
