2f264b12f67a192310f9e68616c6192e2c3579eb
yoonsoo <yoonsoo@hknetworks.kr>
2019-02-28 12:56:57 +0900

    #2.21 Home Controller

---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 31234,
+    title: "video 1",
+    description: "video 1 description",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 11111,
+      name: "yskim",
+      email: "yskimmobile@gmail.com"
+    }
+  },
+  {
+    id: 44321,
+    title: "video 2",
+    description: "video 2 description",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 11111,
+      name: "yskim",
+      email: "yskimmobile@gmail.com"
+    }
+  },
+  {
+    id: 22345,
+    title: "video 3",
+    description: "video 3 description",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 11111,
+      name: "yskim",
+      email: "yskimmobile@gmail.com"
+    }
+  },
+  {
+    id: 55112,
+    title: "video 4",
+    description: "video 4 description",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 11111,
+      name: "yskim",
+      email: "yskimmobile@gmail.com"
+    }
+  }
+];
