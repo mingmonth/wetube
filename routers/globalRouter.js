dd0621d63fac96eb6ff04050ebb20e2dfdb7994f
yoonsoo <yoonsoo@hknetworks.kr>
2019-02-28 16:17:30 +0900

    #2.24 Log In and User Profile Controller

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -4,15 +4,19 @@ import { home, search } from "../controllers/videoController";
 import {
   getJoin,
   postJoin,
-  login,
+  getLogin,
+  postLogin,
   logout
 } from "../controllers/userController";
 const globalRouter = express.Router();
 
-globalRouter.get(routes.home, home);
 globalRouter.get(routes.join, getJoin);
 globalRouter.post(routes.join, postJoin);
-globalRouter.get(routes.login, login);
+
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
+globalRouter.get(routes.home, home);
 globalRouter.get(routes.logout, logout);
 globalRouter.get(routes.search, search);
 
