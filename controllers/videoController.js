3898d755d027b1b1389bc3c903d19a904f84aeea
mingmonth <ys9922@naver.com>
2019-03-04 23:55:41 +0900

    #3.11 Installing ESLint

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -2,7 +2,7 @@ import routes from "../routes";
 import Video from "../models/Video";
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({ _id: -1 });
     res.render("home", { pageTitle: "Home", videos });
   } catch (error) {
     console.log(error);
@@ -15,7 +15,7 @@ export const search = (req, res) => {
   const {
     query: { term: searchingBy }
   } = req;
-  res.render("search", { pageTitle: "Search", searchingBy, videos });
+  res.render("search", { pageTitle: "Search", searchingBy });
 };
 
 //export const videos = (req, res) =>
@@ -82,11 +82,12 @@ export const postEditVideo = async (req, res) => {
 
 export const deleteVideo = async (req, res) => {
   const {
-    params: { id },
+    params: { id }
   } = req;
   try {
     await Video.findOneAndDelete({ _id: id });
-  } catch(error) {}
+  } catch (error) {
+    console.log(error);
+  }
   res.redirect(routes.home);
-}
-  
+};
