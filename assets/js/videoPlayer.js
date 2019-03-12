85bb15f60b718ef2f80dc7c0c97d059658f85f77
yoonsoo <yoonsoo@hknetworks.kr>
2019-03-12 13:00:47 +0900

    #8.2 Mute Unmute

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,17 +1,31 @@
 const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playButton = document.getElementById("jsPlayButton");
+const volumeButton = document.getElementById("jsVolumeButton");
 
 function handlePlayButtonClick() {
   if (videoPlayer.paused) {
     videoPlayer.play();
+    playButton.innerHTML = '<i class="fas fa-pause"></i>';
   } else {
     videoPlayer.pause();
+    playButton.innerHTML = '<i class="fas fa-play"></i>';
+  }
+}
+
+function handleVolumeButtonClick() {
+  if (videoPlayer.muted) {
+    videoPlayer.muted = false;
+    volumeButton.innerHTML = '<i class="fas fa-volume-up"></i>';
+  } else {
+    videoPlayer.muted = true;
+    volumeButton.innerHTML = '<i class="fas fa-volume-mute"></i>';
   }
 }
 
 function init() {
   playButton.addEventListener("click", handlePlayButtonClick);
+  volumeButton.addEventListener("click", handleVolumeButtonClick);
 }
 
 if (videoContainer) {
