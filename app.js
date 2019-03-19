bb5aa1c19e70fbc9d9359bbd5de9fdb304d86bbe
yoonsoo <yoonsoo@hknetworks.kr>
2019-03-19 16:59:39 +0900

    #11.4 Building for Production

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+import "@babel/polyfill";
 import bodyParser from "body-parser";
 import passport from "passport";
 import monogoose from "mongoose";
 import session from "express-session";
+import path from "path";
 import MongoSotre from "connect-mongo";
 import cookieParser from "cookie-parser";
 import express from "express";
@@ -23,8 +25,8 @@ console.log(process.env.COOKIE_SECRET);
 
 app.use(helmet());
 app.set("view engine", "pug");
-app.use("/uploads", express.static("uploads"));
-app.use("/static", express.static("static"));
+app.set("views", path.join(__dirname, "views"));
+app.use("/static", express.static(path.join(__dirname, "static")));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
